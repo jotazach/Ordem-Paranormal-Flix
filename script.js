3350bf903772ad374f26eb67aad666b04e4ca4f2
Jota Zacheo <90703645+jotazach@users.noreply.github.com>
2021-09-27 10:44:45 -0300

    Adicionei os arquivos do Ordem Paranormal Flix

---
+++ script.js
@@ -0,0 +1,159 @@
+var imagens = ["https://i.ytimg.com/vi/uplnCYc0fDg/maxresdefault.jpg", 
+"https://i.ytimg.com/vi/4Ud5QQ2IEgU/maxresdefault.jpg", 
+"https://i.ytimg.com/vi/dtcsHSAEik8/maxresdefault.jpg", 
+"https://i.ytimg.com/vi/23z-tCHgMGI/maxresdefault.jpg", 
+"https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/12aac7c7bff6ad58f1eb_cellbit_1237679905_77415593/thumb/custom-ad7753c1-c87b-44ab-a447-91fdd7a1f757-640x360.jpeg",
+"https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/4aad6c24e58478396873_cellbit_1332449121_83339026/thumb/custom-9c7d3a62-8f2c-4dfa-ab46-ccefdd842e86-640x360.jpeg",
+"https://i.ytimg.com/vi/NhhnGF1Lpx4/maxresdefault.jpg",
+"https://i.ytimg.com/vi/mtXiwMX0K-c/maxresdefault.jpg",
+"https://i.ytimg.com/vi/03Led-EYcA8/maxresdefault.jpg",
+"https://i.ytimg.com/vi/VldAD6_DMgw/maxresdefault.jpg",
+"https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/345c343081829e15e506_cellbit_1788635985_111851899/thumb/custom-1b56b3d5-48ba-4f3c-9344-4c9f41250c2e-640x360.jpeg",
+"https://i.ytimg.com/vi/En2JC9n66bo/maxresdefault.jpg",
+"https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/c1d4ba16b9dcfbe7976c_cellbit_1951247441_122015450/thumb/custom-4b50d5e3-4fd8-465d-91cf-28cbb5c76d98-640x360.jpeg",
+"https://preview.redd.it/y5novh9nqd651.jpg?width=1129&format=pjpg&auto=webp&s=974dbe9da8f5e0e506f15bcfe78dbefb6068132b",
+"https://pbs.twimg.com/media/EbkQkriWoAA4XfF.jpg:large",
+"https://i.ytimg.com/vi/cOTLH2R_iiE/maxresdefault.jpg",
+"https://i.ytimg.com/vi/jVJkjOOFIyw/maxresdefault.jpg",
+"https://i.ytimg.com/vi/o6_N14zB80s/maxresdefault.jpg",
+"https://pbs.twimg.com/media/EdzP39oXYAIJrKc.png",
+"https://i.ytimg.com/vi/b7PvLWZR6pg/maxresdefault.jpg",
+"https://i.ytimg.com/vi/NWcpezJHlb8/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLBAEfRqA1jSHX4cDEmtACni-VnbpA",
+"https://i.ytimg.com/vi/NDq5ETNosdA/maxresdefault.jpg",
+"https://i.ytimg.com/vi/sn4B_3KyzYw/maxresdefault.jpg",
+"https://i.ytimg.com/vi/3XoyEwGk_MA/maxresdefault.jpg",
+"https://i.ytimg.com/vi/LepK245_uUw/maxresdefault.jpg",
+"https://i.ytimg.com/vi/Bi0KoBhpeCw/maxresdefault.jpg",
+"https://i.ytimg.com/vi/mrJvp64_tL4/maxresdefault.jpg",
+"https://pbs.twimg.com/media/EwOtH4kXIAEY6FZ.png",
+"https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/f6672d05cdc532ebeabc_cellbit_41001470413_1615668348/thumb/custom-b6a4118b-29b7-4935-879e-bde9d30a11b4-640x360.png",
+"https://i.ytimg.com/vi/lkjp80bSj2g/maxresdefault.jpg",
+"https://pbs.twimg.com/media/Exg5naRXEAsf6VU.jpg",
+"https://i.ytimg.com/vi/e7UDrn-wDks/maxresdefault.jpg",
+"https://i.ytimg.com/vi/g9i8d-bSWRA/maxresdefault.jpg",
+"https://i.ytimg.com/vi/ta4x_cV5OJw/maxresdefault.jpg",
+"https://i.ytimg.com/vi/kqF7svnqhy0/maxresdefault.jpg",
+"https://i.ytimg.com/vi/nAqPmCAuWxo/maxresdefault.jpg",
+"https://i.ytimg.com/vi/ZEpMspaVfDw/mqdefault.jpg",
+"https://static-cdn.jtvnw.net/cf_vods/d2nvs31859zcd8/ecf1002979744c1b94c2_cellbit_41890498733_1621112086/thumb/custom-81c99f64-e82c-458d-8b6e-e0c8df493771-640x360.png",
+"https://i.ytimg.com/vi/oTCZaA_cHcs/maxresdefault.jpg",
+"https://i.ytimg.com/vi/d7TleOmBKg4/maxresdefault.jpg",
+"https://i.ytimg.com/vi/TRSlNehdWzs/mqdefault.jpg","https://static-cdn.jtvnw.net/cf_vods/d1m7jfoe9zdc1j/3390bc475cf21e4cddb9_cellbit_43747351101_1632000381//thumb/thumb0-320x180.jpg"]
+
+var trailers = ["https://www.youtube.com/watch?v=uplnCYc0fDg&list=PL7ZwE005lvhrBCqdHc3zQKoqzkklKUhN0",
+"https://www.youtube.com/watch?v=4Ud5QQ2IEgU&list=PL7ZwE005lvhrBCqdHc3zQKoqzkklKUhN0&index=2",
+"https://www.youtube.com/watch?v=dtcsHSAEik8&list=PL7ZwE005lvhrBCqdHc3zQKoqzkklKUhN0&index=3",
+"https://www.youtube.com/watch?v=23z-tCHgMGI&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF",
+"https://www.youtube.com/watch?v=sq37bBDs8vk&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=2",
+"https://www.youtube.com/watch?v=qzUD-387XhM&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=3",
+"https://www.youtube.com/watch?v=NhhnGF1Lpx4&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=4",
+"https://www.youtube.com/watch?v=mtXiwMX0K-c&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=5",
+"https://www.youtube.com/watch?v=03Led-EYcA8&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=6",
+"https://www.youtube.com/watch?v=VldAD6_DMgw&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=7",
+"https://www.youtube.com/watch?v=gYdRp7eohyQ&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=8",
+"https://www.youtube.com/watch?v=En2JC9n66bo&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=9",
+"https://www.youtube.com/watch?v=ZCY9Ez6gzfY&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=10",
+"https://www.youtube.com/watch?v=lPEH0PmDA9M&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=11",
+"https://www.youtube.com/watch?v=Rjkt4KpNpBM&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=12",
+"https://www.youtube.com/watch?v=cOTLH2R_iiE&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=13",
+"https://www.youtube.com/watch?v=jVJkjOOFIyw&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=14",
+"https://www.youtube.com/watch?v=o6_N14zB80s&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=15",
+"https://www.youtube.com/watch?v=BH-yFQo882w&list=PL7ZwE005lvhoGkp88Ur7MBL3jXf4XqMjF&index=16",
+"https://www.youtube.com/watch?v=b7PvLWZR6pg&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo",
+"https://www.youtube.com/watch?v=NWcpezJHlb8&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=2",
+"https://www.youtube.com/watch?v=NDq5ETNosdA&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=3",
+"https://www.youtube.com/watch?v=sn4B_3KyzYw&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=4",
+"https://www.youtube.com/watch?v=3XoyEwGk_MA&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=5",
+"https://www.youtube.com/watch?v=LepK245_uUw&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=6",
+"https://www.youtube.com/watch?v=Bi0KoBhpeCw&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=7",
+"https://www.youtube.com/watch?v=mrJvp64_tL4&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=8",
+"https://www.youtube.com/watch?v=N0jF0svrc7w&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=9",
+"https://www.youtube.com/watch?v=epdl81SKm6o&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=10",
+"https://www.youtube.com/watch?v=lkjp80bSj2g&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=11",
+"https://www.youtube.com/watch?v=7Nticvm0PbM&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=12",
+"https://www.youtube.com/watch?v=e7UDrn-wDks&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=13",
+"https://www.youtube.com/watch?v=g9i8d-bSWRA&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=14",
+"https://www.youtube.com/watch?v=ta4x_cV5OJw&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=15",
+"https://www.youtube.com/watch?v=kqF7svnqhy0&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=16",
+"https://www.youtube.com/watch?v=nAqPmCAuWxo&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=17",
+"https://www.youtube.com/watch?v=ZEpMspaVfDw&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=18",
+"https://www.youtube.com/watch?v=reGd6QjPc9s&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=19",
+"https://www.youtube.com/watch?v=oTCZaA_cHcs&list=PL7ZwE005lvhoeU1boBxblL0t_JL1B0Bfo&index=20",
+"https://www.youtube.com/watch?v=d7TleOmBKg4",
+"https://www.youtube.com/watch?v=TRSlNehdWzs","https://www.youtube.com/watch?v=YN4QATSyahs"]
+var generos = [0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3]
+var i = 0
+var localFilme = document.getElementById("localFilme")
+var localHumor = document.getElementById("localFilme2")
+var localFilme3 = document.getElementById("localFilme3")
+var localFilme4 = document.getElementById("localFilme4")
+
+renderizar()
+
+var genero = document.querySelector("select").value
+function renderTemp(){
+    genero = document.querySelector("select").value
+    return genero
+}
+
+function addOnList(){
+    var novaImagem = document.getElementById("inputImagem")
+    var novoTrailer = document.getElementById("inputTrailer")
+    if(novaImagem.value != "" & novoTrailer.value != ""){
+        if(genero == "1"){
+            generos.push(0)
+        }else if(genero == "2"){
+            generos.push(1)
+        }
+        else if(genero == "3"){
+            generos.push(2)
+        }
+        else if(genero == "4"){
+            generos.push(3)
+        }
+        imagens.push(novaImagem.value)
+        trailers.push(novoTrailer.value)
+        novaImagem.value = ""
+        novoTrailer.value = ""
+        renderizar()
+    }
+}
+
+function renderizar(){
+    for(i; i < imagens.length; i++){
+        //localFilme.innerHTML += `<img class="poster" src="${imagens[i]}">`
+        imagem = document.createElement("img")
+        imagem.setAttribute("src", `${imagens[i]}`)
+        botaoTrailer = document.createElement("button")
+        //localFilme.appendChild(botaoTrailer)
+        container = document.createElement("div")
+        container.setAttribute("class", "container")
+        container.appendChild(botaoTrailer)
+        container.appendChild(imagem)
+        localFilme.appendChild(container)// += container
+        botaoTrailer.appendChild(document.createTextNode("Assistir"))
+        botaoTrailer.setAttribute("class", "botao-trailer")
+        botaoTrailer.setAttribute("onclick",`abrirTrailer("${trailers[i]}")`)
+        if(generos[i] == 0){
+      localFilme.appendChild(container)
+        }
+      if(generos[i] == 1) { localHumor.appendChild(container)
+        } 
+      if(generos[i] == 2) { localFilme3.appendChild(container)
+    } 
+      if(generos[i] == 3) {
+     localFilme4.appendChild(container)
+    }
+    }
+}
+
+function abrirTrailer(trailer){
+    var trailerPopUp = trailer.replace("watch?v=", "embed/")
+    trailerPopUp = trailerPopUp.split("&")
+    swal.fire({
+        html: `<iframe width="560" height="315" src="${trailerPopUp[0]}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        width: "fit-content",
+        background: "#343a40",
+        confirmButtonColor: "#dc3545",
+    })
+}
